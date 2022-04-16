d0d3b0bf1809194a2e327810746e554751045b63
nozaki-ais <72852816+nozaki-ais@users.noreply.github.com>
2022-04-16 12:18:42 +0900

    Update task_1.cpp

--- src/task_1.cpp
+++ src/task_1.cpp
@@ -10,7 +10,7 @@
 /**********************************************************************
    Include Libraries
 **********************************************************************/
-#include <opencv/cv.h>
+#include <opencv/cv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
